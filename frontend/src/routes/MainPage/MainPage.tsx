2d7bba0fc4235c4c0d080977d0275792fd280afa
MatsveiDubaleka <matvey.duboleko@yandex.ru>
2024-06-17 19:13:52 +0300

    feat: change image

--- frontend/src/routes/MainPage/MainPage.tsx
+++ frontend/src/routes/MainPage/MainPage.tsx
@@ -55,6 +55,7 @@ export default function App() {
     setTimeout(() => {
       setStep(1);
     }, 2000);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
